62d7fbc7b027b50aa687a6ff8a9598852956abc0
Giulio Zanchetta <zank@outlook.it>
2021-03-03 21:08:59 +0100

    add bk

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '66dd565dc09401589578a2b7a61ef37f'),
+    },
+  },
+});
